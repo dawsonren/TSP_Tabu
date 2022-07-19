3510ddd47f53f4fada3920e523c8ef84d0cb41b9
TheEasyLemon <dawsonren@gmail.com>
2022-07-18 19:01:22 -0500

    Add Tabu search

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ Travelling Salesman Problem with Tabu Search
 
 Dawson Ren, July 15th, 2022
 '''
-from tsp import TSP, LocalSearch, TSPSolver
+from tsp import TSP, LocalSearch, TabuSearch, TSPSolver
 
 if __name__ == '__main__':
-    tsp = TSP(20)
+    N = 30
+    tsp = TSP(N)
     ls = LocalSearch()
+    ts = TabuSearch()
     solver = TSPSolver()
 
     solver.solve(tsp, ls)
+    tsp.path = list(range(30))
+    solver.solve(tsp, ts)
--- tsp.py
+++ tsp.py
@@ -5,6 +5,8 @@ from typing import List, Tuple, Set
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import collections as mc
+from collections import deque
+import random
 
 from interfaces import Problem, SolutionProtocol, Solver
 
@@ -107,7 +109,6 @@ class LocalSearch(SolutionProtocol):
     def search(self, problem: Problem, starting_solution) -> None:
         best_sol = starting_solution
         best_candidate = starting_solution
-
         while not self.stoppingCondition():
             self.iters += 1
             neighborhood = problem.find_neighbors(best_sol)
@@ -120,7 +121,45 @@ class LocalSearch(SolutionProtocol):
             if problem.cost(best_candidate) < problem.cost(best_sol):
                 best_sol = best_candidate
                 print(f'Solution was improved! {best_sol}')
+            else:
+                print('Stuck in a local minima...')
+                break
         
+        return best_sol, problem.cost(best_sol)
+
+
+class TabuSearch(SolutionProtocol):
+    def __init__(self) -> None:
+        self.max_iter = 100
+        self.iters = 0
+        self.max_tabu_size = 1000
+
+    def stoppingCondition(self) -> bool:
+        return self.iters > self.max_iter
+
+    def search(self, problem: Problem, starting_solution) -> None:
+        best_sol = starting_solution
+        best_candidate = starting_solution
+        tabu_list = deque([starting_solution], self.max_tabu_size)
+
+        while not self.stoppingCondition():
+            self.iters += 1
+            neighborhood = problem.find_neighbors(best_sol)
+            best_candidate = random.sample(neighborhood, 1)[0]
+
+            for candidate in neighborhood:
+                if problem.cost(candidate) < problem.cost(best_candidate) and not candidate in tabu_list:
+                    best_candidate = candidate
+            
+            if problem.cost(best_candidate) < problem.cost(best_sol):
+                best_sol = best_candidate
+                print(f'Solution was improved! {best_sol}')
+            
+            tabu_list.append(best_candidate)
+
+            if len(tabu_list) > self.max_tabu_size:
+                tabu_list.popleft()
+
         return best_sol, problem.cost(best_sol)
 
 
